6491a1dca9d2a13a15ed0a7999ccfbf9ac3f48bf
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:03:27 +0200

    Add project files.

---
+++ uppgift 2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+
+        static void Main(string[]args)
+        {
+
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            double dagar= double.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine(" Hur många kilometer vill du hyra bilen?");
+
+            double km = double.Parse(Console.ReadLine());
+
+            double hyra = 300 + km + ((dagar - 1) * 500);
+            Console.WriteLine("Hyran blir " + hyra);
+
+            Console.ReadKey();
+        }
+    }
+}
